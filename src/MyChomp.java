270db2bb9f8d7a84d04a1213f54e24c6d7095cd4
jacksonfava <jackson_fava28@milton.edu>
2026-02-25 11:26:38 -0500

    hi////

---
+++ src/MyChomp.java
@@ -0,0 +1,17 @@
+public class MyChomp {
+    public static void main(String[] args) {
+
+    }
+
+
+    //todo: print out all of the 3x3 boards
+    //no souting 19 times
+
+
+    public MyChomp(){
+        //System.out.println("3.3.3");
+       // for loop to print it out
+        //maybe do an if = 3.3.0 -0.1.0 etc.
+        // for all boards determine if 1.0.0 is one move away
+    }
+}
