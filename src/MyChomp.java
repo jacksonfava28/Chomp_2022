00070c305002b02cdd0a58ca88eb4ed7d4c2a123
jacksonfava <jackson_fava28@milton.edu>
2026-03-06 11:32:37 -0500

    i23hefwokhfiewo

--- src/MyChomp.java
+++ src/MyChomp.java
@@ -1,4 +1,8 @@
+import java.util.HashMap;
+
 public class MyChomp {
+
+    static HashMap<String, Boolean> memo = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -6,50 +10,61 @@ public class MyChomp {
             for (int c2 = c1; c2 >= 0; c2--) {
                 for (int c3 = c2; c3 >= 0; c3--) {
 
-                    if (c1 != 0 || c2 != 0 || c3 != 0) {
+                    if (c1 == 0 && c2 == 0 && c3 == 0) continue;
 
-                        if (oneMoveFrom100(c1, c2, c3)) {
-                            System.out.println(c1 + "." + c2 + "." + c3 +
-                                    " is one move from 1.0.0");
-                        }
+                    boolean win = isWinning(c1, c2, c3);
 
+                    if (win) {
+                        System.out.println(c1 + "." + c2 + "." + c3 + " : Winning");
+                    } else {
+                        System.out.println(c1 + "." + c2 + "." + c3 + " : Losing");
                     }
                 }
             }
         }
     }
 
-    public static boolean oneMoveFrom100(int c1, int c2, int c3) {
+    public static boolean isWinning(int c1, int c2, int c3) {
+
+        // base case
+        if (c1 == 1 && c2 == 0 && c3 == 0) {
+            return false; // losing
+        }
+
+        String key = c1 + "." + c2 + "." + c3;
+
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
 
         int[] board = {c1, c2, c3};
 
         // try every possible move
         for (int col = 0; col < 3; col++) {
-
             for (int height = 0; height < board[col]; height++) {
 
                 int[] newBoard = board.clone();
 
-                // simulate the move
                 for (int i = col; i < 3; i++) {
                     newBoard[i] = Math.min(newBoard[i], height);
                 }
 
-                // check if result is 1.0.0
-                if (newBoard[0] == 1 &&
-                        newBoard[1] == 0 &&
-                        newBoard[2] == 0) {
-                    return true;
+                int n1 = newBoard[0];
+                int n2 = newBoard[1];
+                int n3 = newBoard[2];
+
+                // check valid board ordering
+                if (n1 >= n2 && n2 >= n3) {
+
+                    if (!isWinning(n1, n2, n3)) {
+                        memo.put(key, true);
+                        return true;
+                    }
                 }
             }
         }
 
+        memo.put(key, false);
         return false;
     }
 }
-
-// add state to win boards if 1.0.0 is in possible boards
-// todo: define win and lose boards (win is boards that have lose boards in them and lose boards are ones that have win boards in them)
-// todo: add state to win boards if lose boards are in possible boards
-// todo: if all possible boards are win boards add state to lose boards
-// todo: if 1.0.0 is in possible boards = win board, if all boards in possible = win board the state = lose board
