6c9c332e34f04c124cec36113a3af515df287330
jacksonfava <jackson_fava28@milton.edu>
2026-03-11 11:18:33 -0400

    hediohewcioh

--- src/MyChomp.java
+++ src/MyChomp.java
@@ -2,23 +2,39 @@ import java.util.HashMap;
 
 public class MyChomp {
 
-    static HashMap<String, Boolean> memo = new HashMap<>();
+    static HashMap<String, Boolean> boardResults = new HashMap<>();
 
     public static void main(String[] args) {
 
-        for (int c1 = 3; c1 >= 0; c1--) {
-            for (int c2 = c1; c2 >= 0; c2--) {
-                for (int c3 = c2; c3 >= 0; c3--) {
+        // base losing position
+        boardResults.put("1.0.0", false);
+
+        for (int c1 = 0; c1 <= 3; c1++) {
+            for (int c2 = 0; c2 <= c1; c2++) {
+                for (int c3 = 0; c3 <= c2; c3++) {
 
                     if (c1 == 0 && c2 == 0 && c3 == 0) continue;
 
-                    boolean win = isWinning(c1, c2, c3);
+                    String key = c1 + "." + c2 + "." + c3;
 
-                    if (win) {
-                        System.out.println(c1 + "." + c2 + "." + c3 + " : Winning");
-                    } else {
-                        System.out.println(c1 + "." + c2 + "." + c3 + " : Losing");
+                    if (key.equals("1.0.0")) {
+                        System.out.println(key + " : Losing (base case)");
+                        continue;
                     }
+
+                    boolean winning = isWinning(c1, c2, c3);
+
+                    boardResults.put(key, winning);
+
+                    if (winning) {
+                        System.out.println(key + " : Winning");
+                    } else {
+                        System.out.println(key + " : Losing");
+                    }
+
+                    System.out.println("Moves to:");
+                    findNextBoards(c1, c2, c3);
+                    System.out.println();
                 }
             }
         }
@@ -26,20 +42,8 @@ public class MyChomp {
 
     public static boolean isWinning(int c1, int c2, int c3) {
 
-        // base case
-        if (c1 == 1 && c2 == 0 && c3 == 0) {
-            return false; // losing
-        }
-
-        String key = c1 + "." + c2 + "." + c3;
-
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
         int[] board = {c1, c2, c3};
 
-        // try every possible move
         for (int col = 0; col < 3; col++) {
             for (int height = 0; height < board[col]; height++) {
 
@@ -53,24 +57,43 @@ public class MyChomp {
                 int n2 = newBoard[1];
                 int n3 = newBoard[2];
 
-                // check valid board ordering
                 if (n1 >= n2 && n2 >= n3) {
 
-                    if (!isWinning(n1, n2, n3)) {
-                        memo.put(key, true);
-                        return true;
+                    String nextKey = n1 + "." + n2 + "." + n3;
+
+                    Boolean result = boardResults.get(nextKey);
+
+                    if (result != null && result == false) {
+                        return true; // move to losing board
                     }
                 }
             }
         }
 
-        memo.put(key, false);
-        return false;
+        return false; // all moves go to winning boards
+    }
+
+    public static void findNextBoards(int c1, int c2, int c3) {
+
+        int[] board = {c1, c2, c3};
+
+        for (int col = 0; col < 3; col++) {
+            for (int height = 0; height < board[col]; height++) {
+
+                int[] newBoard = board.clone();
+
+                for (int i = col; i < 3; i++) {
+                    newBoard[i] = Math.min(newBoard[i], height);
+                }
+
+                int n1 = newBoard[0];
+                int n2 = newBoard[1];
+                int n3 = newBoard[2];
+
+                if (n1 >= n2 && n2 >= n3) {
+                    System.out.println("  " + n1 + "." + n2 + "." + n3);
+                }
+            }
+        }
     }
 }
-
-
-
-
-// Todo: find all subsequent boards
-// sidebar: put it before the is winning method as to have it work properly
